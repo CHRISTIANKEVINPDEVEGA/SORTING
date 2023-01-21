8855ab9477e268dc30ef9b5f747a31bf16951572
unknown <Christiandevega12@gmail.com>
2023-01-21 18:42:39 +0800

    changed the code

--- merge_sort.py
+++ merge_sort.py
@@ -1,7 +1,7 @@
-def mergeSort(arrayA):
-    itemCounts = len(arrayA)
+def mergeSort(numbers):
+    itemCounts = len(numbers)
     if(itemCounts==1):
-        return arrayA
+        return numbers
      
 
 Numbers = [76, 72, 100, 92, 88, 71, 81, 45, 70, 42]
