00378e6bda55eeb264ddc6468c85d55cd3c55aa6
unknown <Christiandevega12@gmail.com>
2023-01-21 16:54:39 +0800

    change some code

--- selection_sort.py
+++ selection_sort.py
@@ -1,17 +1,18 @@
 '''Main function'''
 def selectionSort(list):
+    itemCount = len(list)
     '''This is the first loop, it is the loop that determines the start of the searching'''
-    for index in range(10):
+    for index in range(itemCount):
         min_position=index
         '''This is the second loop that picks the index position of the value with the lowest value'''
-        for value in range(min_position,10):
+        for value in range(min_position,itemCount):
             if list[value] < list[min_position]:
                 min_position = value
 
         '''This is the code that manually swaps the value of indexes'''
-        new = list[index]
+        temp = list[index]
         list[index] = list[min_position]
-        list[min_position] = new
+        list[min_position] = temp
         print(list)
     
 
