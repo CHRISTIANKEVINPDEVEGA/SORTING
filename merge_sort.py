0417cbe0616d9419a9aa91de087cb6bba12a2070
unknown <Christiandevega12@gmail.com>
2023-01-21 18:37:41 +0800

    if statement that will make the mergesorter stop spliting the array

--- merge_sort.py
+++ merge_sort.py
@@ -1,5 +1,8 @@
 def mergeSort(arrayA):
-
+    itemCounts = len(arrayA)
+    if(itemCounts==1):
+        return arrayA
+     
 
 Numbers = [76, 72, 100, 92, 88, 71, 81, 45, 70, 42]
 mergeSort(Numbers)
