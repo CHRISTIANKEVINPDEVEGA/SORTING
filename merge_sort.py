7ea61e4f1eb99b4814521054c6da26d0b229db1a
unknown <Christiandevega12@gmail.com>
2023-01-21 18:48:01 +0800

    created two array

--- merge_sort.py
+++ merge_sort.py
@@ -3,6 +3,10 @@ def mergeSort(numbers):
     if(itemCounts==1):
         return numbers
      
+    halfOfnumbers = itemCounts//2
+
+    first_half = mergeSort(list[halfOfnumbers:])
+    later_half = mergeSort(list[:halfOfnumbers])
 
 Numbers = [76, 72, 100, 92, 88, 71, 81, 45, 70, 42]
 mergeSort(Numbers)
