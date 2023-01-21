7e9e422088c062600e65403856c260a41d76ed00
unknown <Christiandevega12@gmail.com>
2023-01-21 12:50:46 +0800

    change code again

--- selection_sort.py
+++ selection_sort.py
@@ -1,11 +1,10 @@
 def selectionSort(list):
-    for index in range(len(list)):
-        search_start = index
-        for value in range(search_start,len(list)):
-            least_value = value
-            if least_value < list[search_start]:
-                list[search_start]=least_value
-                print(list)                
+    for index in range(10):
+        position=index
+        for value in range(position,10):
+            print(value)
+
+
 
 
 
@@ -16,4 +15,4 @@ def selectionSort(list):
 
 
 numbers = [76, 72, 100, 92, 88, 71, 81, 45, 70, 42]
-print(selectionSort(numbers))
+selectionSort(numbers)
