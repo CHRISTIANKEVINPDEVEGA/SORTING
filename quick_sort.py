60923f58ffd563985c964b980a5547439a53c3c6
unknown <Christiandevega12@gmail.com>
2023-01-23 20:21:42 +0800

    added more swapping values and change indexes value

--- quick_sort.py
+++ quick_sort.py
@@ -3,9 +3,12 @@ def partition(numbers, lower, higher):
 
     for index in range(lower, higher):
         if numbers[index] <= pivot:
-            numbers[lower], numbers[higher] = numbers[higher], numbers[lower]
+            numbers[lower], numbers[index] = numbers[index], numbers[lower]
             lower = lower + 1
-          
+
+    numbers[lower], numbers[higher] = numbers[higher], numbers[lower]
+    
+    return lower
 
 
 Numbers = [76, 72, 100, 92, 88, 71, 81, 45, 70, 42]
