52ad799c54b16f915340062a6e5bdd97d2397efc
unknown <Christiandevega12@gmail.com>
2023-01-23 18:54:56 +0800

    change code and deleted exes code

--- merge_sort.py
+++ merge_sort.py
@@ -1,35 +1,33 @@
 def mergeSort(numbers):
     itemCounts = len(numbers)
-    if(itemCounts==1):
+    if itemCounts==1 :
         return numbers
      
     halfOfnumbers = itemCounts//2
 
-    first_half = mergeSort(numbers[halfOfnumbers:])
-    later_half = mergeSort(numbers[:halfOfnumbers])
+    fh_numbers = mergeSort(numbers[:halfOfnumbers])
+    lh_numbers = mergeSort(numbers[halfOfnumbers:])
+
+    return merge(fh_numbers,lh_numbers)
 
 def merge(fh_numbers,lh_numbers):
     mergeSorted = []
+    fh_index = 0
+    lh_index = 0
 
     fh_itemCounts=len(fh_numbers)
     lh_itemCounts=len(lh_numbers)
 
-    while fh_itemCounts > 0 and lh_itemCounts > 0:
-        if fh_numbers[0] > lh_numbers[0]:
-            mergeSorted.append(lh_numbers[0])
-            lh_numbers.remove([0])
+    while fh_itemCounts > fh_index and lh_itemCounts > lh_index:
+        if fh_numbers[fh_index] < lh_numbers[lh_index]:
+            mergeSorted.append(fh_numbers[fh_index])
+            fh_index = fh_index + 1
+
         else:
-            mergeSorted.append(fh_numbers[0])
-            fh_numbers.remove([0])
+            mergeSorted.append(lh_numbers[lh_index])
+            lh_index = lh_index + 1
 
-    while  fh_itemCounts > 0:
-        mergeSorted.append(fh_numbers[0])
-        fh_numbers.remove(0)
-
-    while lh_itemCounts > 0:
-        mergeSorted.append(lh_numbers[0])
-        lh_numbers.remove(0)
 
 
 Numbers = [76, 72, 100, 92, 88, 71, 81, 45, 70, 42]
-mergeSort(Numbers)
+print(mergeSort(Numbers))
