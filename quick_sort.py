bf067b94c03d6f926a57dfa5b0df7aadcf7982ec
unknown <Christiandevega12@gmail.com>
2023-01-23 20:16:37 +0800

    added pivot and changed code

--- quick_sort.py
+++ quick_sort.py
@@ -1,7 +1,8 @@
-def quickSort(numbers):
-    pivot = len(numbers)//2
+def partition(numbers, lower, higher):
+    low = lower
+    pivot = numbers[higher]
+    
 
-    
-    
 
 Numbers = [76, 72, 100, 92, 88, 71, 81, 45, 70, 42]
+quickSort(Numbers)
