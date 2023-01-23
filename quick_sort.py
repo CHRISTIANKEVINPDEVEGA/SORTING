3ad486ce170c74b5b475005a9a73223cf64f45f3
unknown <Christiandevega12@gmail.com>
2023-01-23 20:26:14 +0800

    added values inside quicksort function

--- quick_sort.py
+++ quick_sort.py
@@ -13,7 +13,8 @@ def partition(numbers, lower, higher):
 def quickSort(numbers, lower, higher):
     if lower < higher:  
         partition_index = partition(numbers,lower, higher)
-
+        quickSort(numbers,lower, partition_index - 1)
+        quickSort(numbers, partition_index + 1, higher)
 
 Numbers = [76, 72, 100, 92, 88, 71, 81, 45, 70, 42]
 quickSort(Numbers, 0, len(Numbers)-1)
